2778b68b56b12c82ba80caeafb6f6fee79afb86e
Kevin Herro <kvherro@gmail.com>
2023-09-24 09:09:12 -0700

    Initial Commit

---
+++ WhatsTheNuts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhatsTheNuts
+//
+//  Created by Kevin Herro on 9/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhatsTheNuts/WhatsTheNutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsTheNutsApp.swift
+//  WhatsTheNuts
+//
+//  Created by Kevin Herro on 9/24/23.
+//
+
+import SwiftUI
+
+@main
+struct WhatsTheNutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
