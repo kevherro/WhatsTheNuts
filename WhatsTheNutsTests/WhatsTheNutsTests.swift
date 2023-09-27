9dada39998bed3d523fbd51900343741dcefcc20
Kevin Herro <kvherro@gmail.com>
2023-09-27 08:32:04 -0700

    fix tests

--- WhatsTheNutsTests/WhatsTheNutsTests.swift
+++ WhatsTheNutsTests/WhatsTheNutsTests.swift
@@ -12,46 +12,46 @@ import XCTest
 final class WhatsTheNutsTests: XCTestCase {
   func testRoyalFlush() {
     let communityCards = [
-      Card(rank: .ten, suit: .club),
-      Card(rank: .jack, suit: .club),
-      Card(rank: .queen, suit: .club),
-      Card(rank: .king, suit: .club),
-      Card(rank: .ace, suit: .club),
+      Card(rank: .ten, suit: .clubs),
+      Card(rank: .jack, suit: .clubs),
+      Card(rank: .queen, suit: .clubs),
+      Card(rank: .king, suit: .clubs),
+      Card(rank: .ace, suit: .clubs),
     ]
 
     let roundController = RoundController(communityCards: communityCards)
     var nutsController = NutsController(round: roundController)
 
-    XCTAssertEqual(nutsController.strongestHand, HandStrength.royalFlush)
+    XCTAssertEqual(nutsController.strongestHandResult.strength, HandStrength.royalFlush)
   }
 
   func testFourOfAKind() {
     let communityCards = [
-      Card(rank: .ten, suit: .club),
-      Card(rank: .ten, suit: .spade),
-      Card(rank: .seven, suit: .heart),
-      Card(rank: .two, suit: .club),
-      Card(rank: .three, suit: .heart),
+      Card(rank: .ten, suit: .clubs),
+      Card(rank: .ten, suit: .spades),
+      Card(rank: .seven, suit: .hearts),
+      Card(rank: .two, suit: .clubs),
+      Card(rank: .three, suit: .hearts),
     ]
 
     let roundController = RoundController(communityCards: communityCards)
     var nutsController = NutsController(round: roundController)
 
-    XCTAssertEqual(nutsController.strongestHand, HandStrength.fourOfAKind)
+    XCTAssertEqual(nutsController.strongestHandResult.strength, HandStrength.fourOfAKind)
   }
 
   func testStraightFlush() {
     let communityCards = [
-      Card(rank: .two, suit: .club),
-      Card(rank: .two, suit: .spade),
-      Card(rank: .five, suit: .club),
-      Card(rank: .six, suit: .club),
-      Card(rank: .nine, suit: .heart),
+      Card(rank: .two, suit: .clubs),
+      Card(rank: .two, suit: .spades),
+      Card(rank: .five, suit: .clubs),
+      Card(rank: .six, suit: .clubs),
+      Card(rank: .nine, suit: .hearts),
     ]
 
     let roundController = RoundController(communityCards: communityCards)
     var nutsController = NutsController(round: roundController)
 
-    XCTAssertEqual(nutsController.strongestHand, HandStrength.straightFlush)
+    XCTAssertEqual(nutsController.strongestHandResult.strength, HandStrength.straightFlush)
   }
 }
